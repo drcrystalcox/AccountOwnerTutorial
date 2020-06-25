c98c8c1b85fcd7b11ce85f068101f47f070ed1a5
Dylan Houston <dylanhouston7@outlook.com>
2020-06-25 11:35:36 -0400

    Added Swagger to project

--- AccountOwnerApi/Startup.cs
+++ AccountOwnerApi/Startup.cs
@@ -1,20 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using AccountOwnerApi.Extensions;
 using NLog;
 using System.IO;
 using Microsoft.AspNetCore.HttpOverrides;
 using AutoMapper;
+using Microsoft.OpenApi.Models;
 
 namespace AccountOwnerApi
 {
@@ -43,6 +38,15 @@ namespace AccountOwnerApi
 
             services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+	            c.SwaggerDoc("v1", new OpenApiInfo()
+	            {
+		            Version = "1"
+	            });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +56,17 @@ namespace AccountOwnerApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+	            c.SwaggerEndpoint("/swagger/v1/swagger.json", "AccountOwner API V1");
+	            c.RoutePrefix = string.Empty;
+            });
+
 
             app.UseHttpsRedirection();
 
